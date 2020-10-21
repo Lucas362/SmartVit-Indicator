101dfd8bf568cac7af4c2127b8fe503d87d80d51
Lucas362 <lucasvitorifg@gmail.com>
2020-10-20 23:45:32 -0300

    [Feature] Starting indicators

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,4 @@
 from flask_pymongo import PyMongo, MongoClient
 
-client = MongoClient('mongodb://service-mongodb:27017/', username='admin', password='password')
+client = MongoClient('mongodb://indicators-mongodb:27017/', username='admin', password='password')
 db = client.smartDevApiService
