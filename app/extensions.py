b665ff7c4d7de020fa9a04a01aeb2a4538d6d045
Luciano Santos <luciano_z7@hotmail.com>
2020-10-26 23:26:41 -0300

    [Feature] Adding heroku file and env configuration

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,20 @@
 from flask_pymongo import MongoClient
+from settings import load_database_params
+import pymongo
+import os
 
+password = os.getenv("MONGOPASSWORD")
+dbname = os.getenv("DBNAME", "smart-dev")
+env = os.getenv("ENVIRONMENT")
 
-client = MongoClient(
-    'mongodb://indicators-mongodb:27017/',
-    username='admin',
-    password='password'
-)
-db = client.smartDevApiService
+if password and dbname:
+    client = MongoClient(
+        "mongodb+srv://smartAdmin:"
+        + password + "@smartvit.6s6r9.mongodb.net/"
+        + dbname + "?retryWrites=true&w=majority"
+    )
+
+else:
+    client = pymongo.MongoClient(
+        **load_database_params(), serverSelectionTimeoutMS=10
+    )
